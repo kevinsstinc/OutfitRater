3dca1cd654cd671f04f51677baf3673e367efff9
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2025-08-30 15:17:42 +0800

    Initial Commit

---
+++ OutfitRater/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OutfitRater
+//
+//  Created by Joseph Kevin Fredric on 30/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OutfitRater/OutfitRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutfitRaterApp.swift
+//  OutfitRater
+//
+//  Created by Joseph Kevin Fredric on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct OutfitRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
